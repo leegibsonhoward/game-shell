e4318a9995ca022877c79756e3cc9b5d8b4fd163
Lee Howard <leegibsonhoward@gmail.com>
2025-05-24 16:23:04 -0700

    add enemy tracking player ai behavior example
    - disable for now to keep engine flexible

--- src/systems/EnemySystem.js
+++ src/systems/EnemySystem.js
@@ -13,8 +13,30 @@ export default class EnemySystem {
   }
 
   updateAllEnemies(deltaTime) {
+	// enemy tracking player is disabled for now to
+	// keep engine capabilities flexible
+    // this.trackPlayer();
     for (const enemy of this.entityManager.getEnemies()) {
       if (enemy.update) enemy.update(deltaTime);
-    }
+	}
   }
+  
+  // example of extending enemy system with ai behavior
+  // TODO: Branch out into separate modules to keep lean
+  // and only have them here for now?
+  trackPlayer(deltaTime) {
+    const player = this.entityManager.getPlayer();
+    for (const enemy of this.entityManager.getEnemies()) {
+      const dx = player.x - enemy.x;
+      const dy = player.y - enemy.y;
+      const dist = Math.hypot(dx, dy);
+
+      const speed = 1;
+      if (dist > 0) {
+        enemy.x += (dx / dist) * speed;
+        enemy.y += (dy / dist) * speed;
+      }
+    }
+	}
+
 }
